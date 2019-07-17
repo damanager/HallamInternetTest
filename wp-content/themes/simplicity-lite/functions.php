f245454bd3b888971e97d78d010470be2aed982d
ISAIAH OKE{Da-Manager} <web@da-manager.com>
2019-07-17 01:03:42 +0100

    Delete unused themes
    Had to delete un-used theme to avoid confusion also attached sql table to show data in file

--- wp-content/themes/simplicity-lite/functions.php
+++ wp-content/themes/simplicity-lite/functions.php
@@ -240,7 +240,6 @@ function tt_enqueue_scripts(): void {
 
 	wp_enqueue_script(
 		'site-main',
-		get_template_directory() . '/main.js', // Use 'get_template_director_uri()' method.
 		get_template_directory_uri () . '/main.js', // Use 'get_template_director_uri()' method.
 		[ 'jquery' ],
 		filemtime( get_template_directory() . '/main.js' ),
